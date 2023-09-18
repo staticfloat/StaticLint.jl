4a4af364827d5bde12694775cbd0701c881ed324
pfitzseb <pfitzseb@users.noreply.github.com>
2023-09-18 15:57:53 +0000

    Format files using DocumentFormat

--- docs/make.jl
+++ docs/make.jl
@@ -7,16 +7,16 @@ makedocs(;
     repo="https://github.com/julia-vscode/StaticLint.jl/blob/{commit}{path}#L{line}",
     sitename="StaticLint.jl",
     format=Documenter.HTML(;
-        prettyurls=prettyurls = get(ENV, "CI", nothing) == "true",
+        prettyurls=prettyurls = get(ENV, "CI", nothing) == "true"
         # canonical="https://www.julia-vscode.org/StaticLint.jl",
         # assets=String[],
     ),
     pages=[
         "Home" => "index.md",
         "Syntax Reference" => "syntax.md",
-    ],
+    ]
 )
 
 deploydocs(;
-    repo="github.com/julia-vscode/StaticLint.jl",
+    repo="github.com/julia-vscode/StaticLint.jl"
 )
